76b3659df6951a78e395d8844d484569f39b32e1
Stephen <svia3@gatech.edu>
2020-07-15 13:23:47 -0400

    testing multiple write

--- main.c
+++ main.c
@@ -77,6 +77,7 @@ int main()
     // printf("Read Multiple:        %hhu\n", buffer_read_multiple(read_buf, &test_buf, 10));
     // print_arr(read_buf, 200);
 //
+    int write_bytes = 10;
     printf("Read Multiple:        %hhu\n", buffer_read_multiple(read_buf, &test_buf, 20));
     // print_arr(read_buf, 200);
     print_buffer_stats(&test_buf);
@@ -85,16 +86,18 @@ int main()
     // buffer_flush(&test_buf);
     // printf("Write one byte:       %zu\n", buffer_write(&test_buf, 22));
     // print_buffer_stats(&test_buf);
-    printf("Write multiple bytes: %zu\n", buffer_write_multiple(&test_buf, overflow_arr, 10));
+    printf("Write multiple bytes: %zu\n", buffer_write_multiple(&test_buf, overflow_arr, write_bytes));
     print_buffer_stats(&test_buf);
     for(int i = 0; i < 200; ++i) {
         overflow_arr[i] = 2;
     }
-    printf("Write multiple bytes: %zu\n", buffer_write_multiple(&test_buf, overflow_arr, 5));
+    write_bytes = 5;
+    printf("Wanted to write: %d; Wrote multiple bytes: %zu\n", write_bytes, buffer_write_multiple(&test_buf, overflow_arr, write_bytes));
     print_buffer_stats(&test_buf);
-    // printf("Write multiple bytes: %zu\n", buffer_write_multiple(&test_buf, overflow_arr, 50));
-    // printf("Write multiple bytes: %zu\n", buffer_write_multiple(&test_buf, overflow_arr, 50));
-    // print_buffer_stats(&test_buf);
+
+    write_bytes = 15;
+    printf("Wanted to write: %d; Wrote multiple bytes: %zu\n", write_bytes, buffer_write_multiple(&test_buf, overflow_arr, write_bytes));
+    print_buffer_stats(&test_buf);
     // buffer_flush(&test_buf);
     // printf("Write multiple bytes: %zu\n", buffer_write_multiple(&test_buf, overflow_arr, 10));
     // buffer_read_multiple(read_buf, &test_buf, 10);
